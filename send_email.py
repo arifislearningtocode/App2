9ce91ba3fb7b2f6b98695225769f6292f04571dd
abdulrehman.arif <coolabdulrehman3030@gmail.com>
2023-08-02 18:27:20 +0500

    contact us of challenge done dropdown added

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,8 @@ def send_email(message):
     port = 456
 
     username = 'coolabdulrehman3030@gmail.com'
-    password = 'igpuipazijglmxep'
+    password = os.getenv("PASSWORD")
+    # password = 'igpuipazijglmxep'
 
     receiver = 'coolabdulrehman3030@gmail.com'
     context = ssl.create_default_context()
